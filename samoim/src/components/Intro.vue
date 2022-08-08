6c3edc85a07a63cadd5be447cbac214cbce24f9f
Lim Hyun <hyunzion@gmail.com>
2022-08-09 00:29:34 +0900

    feat: Intro.vue
    Resolve: MEMB-001

--- samoim/src/components/Intro.vue
+++ samoim/src/components/Intro.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container>
+  <v-container class="bg">
+    <div style="height: 750px;"></div>
 
-    {{ test }}
+    <template>
+        <v-btn
+            block
+            dark
+            color="#FFBC00"
+        >
+            스타뱅킹으로 5초만에 시작하기
+        </v-btn>
+    </template>
+
+    <template>
+        <v-btn block>
+            다른 방법으로 시작하기
+        </v-btn>
+    </template>
 
   </v-container>
 </template>
@@ -15,3 +30,11 @@
     }),
   }
 </script>
+
+<style scoped>
+.bg {
+  background: url('../assets/intro.png');
+  background-size: cover;
+  height: 100vh;
+}
+</style>
