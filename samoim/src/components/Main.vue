50930db0ad12a0a4f026b131fd442be0ca1dbbd6
youngjin <youngjin8227@gmail.com>
2022-08-08 15:36:17 +0900

    vuetify apply

---
+++ samoim/src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+
+    <!-- 추천 모임 -->
+    <v-img
+      max-height="300"
+      max-width="414"
+      src="../assets/tennis.png"
+    ></v-img>
+
+    <!-- 모임 리스트 -->
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Main',
+
+    data: () => ({
+      test: 'SAMOIM MAIN PAGE',
+    }),
+  }
+</script>
