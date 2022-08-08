c9d2f92d7fe4692c3a9d9a21ad7ca32eb0ae44a6
Lim Hyun <hyunzion@gmail.com>
2022-08-08 15:52:28 +0900

    feat: Create Intro Vue

---
+++ samoim/src/components/Intro.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+
+    {{ test }}
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Intro',
+
+    data: () => ({
+      test: 'SAMOIM Intro PAGE',
+    }),
+  }
+</script>
