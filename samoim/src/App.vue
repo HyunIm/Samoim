4d5e7ece36e6680cb8e44b1d60ddfe446b811ad0
youngjin <youngjin8227@gmail.com>
2022-08-09 20:27:03 +0900

    main page fillter & search dialog add

--- samoim/src/App.vue
+++ samoim/src/App.vue
@@ -1,36 +1,6 @@
 <template>
   <v-app>
-        <!-- top bar -->
-    <v-row>
-      <v-app-bar
-        :color="color"
-        class=""
-        fixed
-        style="padding:0 5%"
-        flat
-      >
-        <div class="d-flex align-center">
-          <h1>
-            <p class="font-2 mb-n1" :style="`color:#FBC02D`">SAMOIM</p>
-          </h1>
-        </div>
-
-      <v-spacer></v-spacer>
-      <v-icon
-        large
-        color="black darken-2"
-      >
-        mdi-filter-cog-outline
-      </v-icon>
-      <v-icon
-        large
-        color="black darken-2"
-        class="ml-2"
-      >
-        mdi-magnify
-      </v-icon>
-      </v-app-bar>
-    </v-row>
+    <!-- top bar -->
 
     <!-- Main View -->
     <v-main>
@@ -38,49 +8,7 @@
     </v-main>
 
     <!-- bottom bar -->
-    <v-footer padless fixed=true>
-        <v-bottom-navigation v-model="value" dark background-color="#FBC02D">
-          <router-link 
-            style="text-decoration: none; color: inherit;" 
-            to="/main" 
-          >
-            <v-btn value="recent" color="#FBC02D" class="mt-1">
-              <span>홈</span>
-              <v-icon>mdi-home</v-icon>
-            </v-btn>
-          </router-link>
-
-          <router-link 
-            style="text-decoration: none; color: inherit;" 
-            to="/make"
-          >
-            <v-btn value="favorites" color="#FBC02D" class="mt-1">
-              <span>만들기</span>
-              <v-icon>mdi-plus-circle-outline</v-icon>
-            </v-btn>
-          </router-link>
-
-          <router-link 
-            style="text-decoration: none; color: inherit;" 
-            to="/mypage"
-          >
-            <v-btn value="nearby" color="#FBC02D" class="mt-1">
-              <span>내 모임</span>
-              <v-icon>mdi-calendar-month</v-icon>
-            </v-btn>
-          </router-link>
-
-          <router-link 
-            style="text-decoration: none; color: inherit;" 
-            to="/profile"
-          >
-            <v-btn value="nearby" color="#FBC02D" class="mt-1">
-              <span>프로필</span>
-              <v-icon>mdi-account</v-icon>
-            </v-btn>
-          </router-link>  
-        </v-bottom-navigation>
-    </v-footer>
+    
 
 
   </v-app>
---
+++ samoim/src/components/common/BottomBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-footer padless fixed=true>
+        <v-bottom-navigation v-model="value" dark background-color="#FBC02D">
+          <router-link 
+            style="text-decoration: none; color: inherit;" 
+            to="/main" 
+          >
+            <v-btn value="recent" color="#FBC02D" class="mt-1">
+              <span>홈</span>
+              <v-icon>mdi-home</v-icon>
+            </v-btn>
+          </router-link>
+
+          <router-link 
+            style="text-decoration: none; color: inherit;" 
+            to="/make"
+          >
+            <v-btn value="favorites" color="#FBC02D" class="mt-1">
+              <span>만들기</span>
+              <v-icon>mdi-plus-circle-outline</v-icon>
+            </v-btn>
+          </router-link>
+
+          <router-link 
+            style="text-decoration: none; color: inherit;" 
+            to="/mypage"
+          >
+            <v-btn value="nearby" color="#FBC02D" class="mt-1">
+              <span>내 모임</span>
+              <v-icon>mdi-calendar-month</v-icon>
+            </v-btn>
+          </router-link>
+
+          <router-link 
+            style="text-decoration: none; color: inherit;" 
+            to="/profile"
+          >
+            <v-btn value="nearby" color="#FBC02D" class="mt-1">
+              <span>프로필</span>
+              <v-icon>mdi-account</v-icon>
+            </v-btn>
+          </router-link>  
+        </v-bottom-navigation>
+    </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'BottomBar',
+
+    data: () => ({
+    }),
+  }
+</script>
