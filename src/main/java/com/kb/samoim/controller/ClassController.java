6cc11832537ddb137dba381d356c5c6d8076a311
sumin95 <suminnnnnnn@naver.com>
2022-08-10 08:59:10 +0900

    모임 목록 조회 rest api 추가

---
+++ src/main/java/com/kb/samoim/controller/ClassController.java
@@ -0,0 +1,36 @@
+package com.kb.samoim.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kb.samoim.dao.ClassDao;
+import com.kb.samoim.model.Class;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class ClassController {
+
+	@Autowired
+	private ClassDao classDao;
+
+	@ApiOperation(value = "모임 목록 정보 가져오기 (모임 생성 최신 순으로 정렬)")
+	@GetMapping(value = "/classes")
+	public ResponseEntity<List<Class>> getClassAll() {
+		List<Class> list = null;
+		try {
+			list = classDao.selectClassAll();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		return new ResponseEntity<List<Class>>(list, HttpStatus.OK);
+	}
+}
