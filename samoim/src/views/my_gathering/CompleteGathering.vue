c727ab00dacee674bf380a79b9b755f48a872336
Lim Hyun <hyunzion@gmail.com>
2022-08-10 11:20:51 +0900

    feat: 내 모임 완료 페이지 개발

--- samoim/src/views/my_gathering/CompleteGathering.vue
+++ samoim/src/views/my_gathering/CompleteGathering.vue
@@ -34,18 +34,116 @@
       </v-btn>
     </router-link>
   </v-bottom-navigation>
+
+
+  <v-card
+    class="mx-auto ma-5"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          등신: 등반의 신
+        </v-list-item-title>
+        &nbsp; • 인원: 7명/15명 <br>
+        &nbsp; • 스포츠 > 등산 <br>
+        &nbsp; • 지역 : 수원(영통구)
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+      >
+        <img src="../../assets/mountaineering.jpeg">
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        상세 페이지
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+
+  <v-card
+    class="mx-auto ma-5"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          테니스의 왕자
+        </v-list-item-title>
+        &nbsp; • 인원: 5명/10명 <br>
+        &nbsp; • 스포츠 > 테니스 <br>
+        &nbsp; • 지역 : 서울(강남구)
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+      >
+        <img src="../../assets/tennis.png">
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        상세 페이지
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+
+  <v-card
+    class="mx-auto ma-5"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          와인 럽
+        </v-list-item-title>
+        &nbsp; • 인원: 4명/6명 <br>
+        &nbsp; • 사교 > 와인 <br>
+        &nbsp; • 지역 : 일산
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+      >
+        <img src="../../assets/wine.jpeg">
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        상세 페이지
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </div>
 </template>
 
 <script>
   export default {
-    data: () => ({
-      alignments: [
-        'start',
-        'center',
-        'end',
-      ],
-    }),
   }
 </script>
 
