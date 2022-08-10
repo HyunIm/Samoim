c7ad39613fa994cd4b30cf06824469e90da414e5
youngjin <youngjin8227@gmail.com>
2022-08-11 01:17:03 +0900

    backend api call

--- samoim/vue.config.js
+++ samoim/vue.config.js
@@ -6,8 +6,9 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        "target" : 'http://169.56.100.108:32487/samoim/',
-        "pathRewrite": {'^/api': ''},
+        "target" : 'http://169.56.100.108:32487/',
+        //"target" : 'http://samoim.kbfg.kubepia.com/',
+        changeOrigin: true,
       }
     }
   }
