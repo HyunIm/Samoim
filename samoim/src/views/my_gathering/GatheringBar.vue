60dd80e34fba277ccbc2de8c828f8efc4f54072c
Lim Hyun <hyunzion@gmail.com>
2022-08-12 14:12:17 +0900

    feat: 내 활동 추가

--- samoim/src/views/my_gathering/GatheringBar.vue
+++ samoim/src/views/my_gathering/GatheringBar.vue
@@ -3,17 +3,30 @@
     <v-tabs
       align-with-title
       color="black">
-      <v-tab>대기</v-tab>
-      <v-tab>참여</v-tab>
-      <v-tab>진행</v-tab>
-      <v-tab>찜</v-tab>
+      <v-tab @click="waitClick">대기</v-tab>
+      <v-tab @click="partClick">참여</v-tab>
+      <v-tab @click="progClick">진행</v-tab>
+      <v-tab @click="wishClick">찜</v-tab>
     </v-tabs>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    waitClick() {
+      this.$emit('waitClick', 'waitClick')
+    },
+    partClick() {
+      this.$emit('partClick', 'partClick')
+    },
+    progClick() {
+      this.$emit('progClick', 'progClick')
+    },
+    wishClick() {
+      this.$emit('wishClick', 'wishClick')
+    },
+  }
 }
 </script>
 
--- samoim/src/views/my_gathering/MyGathering.vue
+++ samoim/src/views/my_gathering/MyGathering.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
+    <br>
     <h2>&nbsp;내 활동</h2>
-    <GatheringBar/>
-    <ProgressGathering/>
-    <ParticipateGathering/>
-    <WaitGathering/>
-    <WishGathering/>
+    <br>
+    <GatheringBar
+      @waitClick="waitClick"
+      @partClick="partClick"
+      @progClick="progClick"
+      @wishClick="wishClick"
+      />
+    <WaitGathering v-if="waitTab"/>
+    <WaitGathering v-if="waitTab"/>
+    <WaitGathering v-if="waitTab"/>
+    <ParticipateGathering v-if="paricipateTab"/>
+    <ProgressGathering v-if="progressTab"/>
+    <ProgressGathering v-if="progressTab"/>
+    <WishGathering v-if="wishTab"/>
     <BottomBar/>
   </div>
 </template>
@@ -13,18 +23,50 @@
 <script>
 import GatheringBar from './GatheringBar.vue'
 import BottomBar from '../../components/common/BottomBar.vue'
-import ProgressGathering from './ProgressGathering.vue'
-import ParticipateGathering from './ParticipateGathering.vue'
 import WaitGathering from './WaitGathering.vue'
+import ParticipateGathering from './ParticipateGathering.vue'
+import ProgressGathering from './ProgressGathering.vue'
 import WishGathering from './WishGathering.vue'
 export default {
+  data: () => ({
+    waitTab: true,
+    paricipateTab: false,
+    progressTab: false,
+    wishTab: false,
+  }),
   components: {
     GatheringBar,
-    ProgressGathering,
     BottomBar,
-    ParticipateGathering,
     WaitGathering,
+    ParticipateGathering,
+    ProgressGathering,
     WishGathering,
+  },
+  methods: {
+    waitClick() {
+      this.waitTab = true
+      this.paricipateTab = false
+      this.progressTab = false
+      this.wishTab = false
+    },
+    partClick() {
+      this.waitTab = false
+      this.paricipateTab = true
+      this.progressTab = false
+      this.wishTab = false
+    },
+    progClick() {
+      this.waitTab = false
+      this.paricipateTab = false
+      this.progressTab = true
+      this.wishTab = false
+    },
+    wishClick() {
+      this.waitTab = false
+      this.paricipateTab = false
+      this.progressTab = false
+      this.wishTab = true
+    },
   }
 }
 </script>
