a170cb252244159a4db64a48c374fa901643397b
Lim Hyun <hyunzion@gmail.com>
2022-08-17 14:08:40 +0900

    fix: 이전 버전 복구

--- samoim/src/store/index.js
+++ samoim/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import createPersistedState from "vuex-persistedstate";
+// import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -28,5 +28,5 @@ export default new Vuex.Store({
   actions: {},
   modules: {},
 
-  plugins: [createPersistedState({})],
+  // plugins: [createPersistedState({})],
 });
