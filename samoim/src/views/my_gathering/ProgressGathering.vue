d70e09fe1016234719110d09121b3bab3ae857bf
youngjin <youngjin8227@gmail.com>
2022-08-18 15:10:24 +0900

    모임 이미지 바인딩

--- samoim/src/views/my_gathering/ProgressGathering.vue
+++ samoim/src/views/my_gathering/ProgressGathering.vue
@@ -13,15 +13,15 @@
           <v-list-item-avatar
             tile
             size="80"
-            color="grey"
           >
+          <v-img :src="item.photoPath"></v-img>
           </v-list-item-avatar>
           <v-list-item-content>
             <div class="text-overline">
-              {{ item.LARGE_CATEGORY }} > {{ item.SMALL_CATEGORY }}
+              {{ item.largeCategory }} > {{ item.smallCategory }}
             </div>
             <v-list-item-title class="text-h5 mb-1">
-              {{ item.NAME }}
+              {{ item.name }}
             </v-list-item-title>
               <v-row>
                 &nbsp;
@@ -43,7 +43,7 @@
                     mdi-map-marker-outline
                   </v-icon>
                   <small>
-                    {{ item.CITY }} {{ item.ADDRESS }} 
+                    {{ item.city }} {{ item.address }} 
                   </small>
                 </v-chip>
 
@@ -57,7 +57,7 @@
                     mdi-account-group
                   </v-icon>
                   <small>
-                    {{item.CURRENTMEMBER}}/{{ item.MAX_MEMBER }}
+                    {{item.currentmember}}/{{ item.maxMember }}
                   </small>
                 </v-chip>
               </v-row>
@@ -79,7 +79,7 @@
             <v-col>
               <router-link
                 style="text-decoration: none; color: inherit;" 
-                :to="{name: 'GatheringInformation', params: {classId: item.ID}}"
+                :to="{name: 'GatheringInformation', params: {element: item}}"
               >
               <v-btn
                 outlined
@@ -113,11 +113,53 @@
 <script>
 import Empty from '@/assets/starfriends/empty2.png';
 
+import climbingImg from '@/assets/class_img/0_climbing.png';
+import golfImg from '@/assets/class_img/1_golf.png';
+import tennisImg from '@/assets/class_img/2_tennis.png';
+import driveImg from '@/assets/class_img/3_drive.png';
+import campingImg from '@/assets/class_img/4_camping.png';
+import concertImg from '@/assets/class_img/5_concert.png';
+import musicalImg from '@/assets/class_img/6_musical.png';
+import exhibitionImg from '@/assets/class_img/7_exhibition.png';
+import bandImg from '@/assets/class_img/8_band.png';
+import compositionImg from '@/assets/class_img/9_composition.png';
+import drawingImg from '@/assets/class_img/10_drawing.png';
+import writingImg from '@/assets/class_img/11_writing.png';
+import readingImg from '@/assets/class_img/12_reading.png';
+import studyImg from '@/assets/class_img/13_study.png';
+import foreignImg from '@/assets/class_img/14_foreign.png';
+import dogImg from '@/assets/class_img/15_dog.png';
+import donationImg from '@/assets/class_img/16_donation.png';
+import cookingImg from '@/assets/class_img/17_cooking.png';
+import dessertImg from '@/assets/class_img/18_dessert.png';
+import sojuImg from '@/assets/class_img/19_soju.png';
+
 export default {
   data: () => ({
     classData: [],
     empty: Empty,
     isEmpty: false,
+
+    climbingImg : climbingImg,
+    golfImg : golfImg,
+    tennisImg : tennisImg,
+    driveImg : driveImg,
+    campingImg : campingImg,
+    concertImg : concertImg,
+    musicalImg : musicalImg,
+    exhibitionImg : exhibitionImg,
+    bandImg : bandImg,
+    compositionImg : compositionImg,
+    drawingImg : drawingImg,
+    writingImg : writingImg,
+    readingImg : readingImg,
+    studyImg : studyImg,
+    foreignImg : foreignImg,
+    dogImg : dogImg,
+    donationImg : donationImg,
+    cookingImg : cookingImg,
+    dessertImg : dessertImg,
+    sojuImg : sojuImg,
   }),
 
   mounted() {
@@ -132,6 +174,48 @@ export default {
 
         if(this.classData.length > 0) {
           this.isEmpty = false;
+
+          for(var i=0; i<this.classData.length; i++) {
+            if(this.classData[i].photoPath === "climbingImg"){
+              this.classData[i].photoPath = this.climbingImg;
+            } else if(this.classData[i].photoPath === "golfImg"){
+              this.classData[i].photoPath = this.golfImg;
+            } else if(this.classData[i].photoPath === "tennisImg"){
+              this.classData[i].photoPath = this.tennisImg;
+            } else if(this.classData[i].photoPath === "driveImg"){
+              this.classData[i].photoPath = this.driveImg;
+            } else if(this.classData[i].photoPath === "concertImg"){
+              this.classData[i].photoPath = this.concertImg;
+            } else if(this.classData[i].photoPath === "musicalImg"){
+              this.classData[i].photoPath = this.musicalImg;
+            } else if(this.classData[i].photoPath === "exhibitionImg"){
+              this.classData[i].photoPath = this.exhibitionImg;
+            } else if(this.classData[i].photoPath === "bandImg"){
+              this.classData[i].photoPath = this.bandImg;
+            } else if(this.classData[i].photoPath === "compositionImg"){
+              this.classData[i].photoPath = this.compositionImg;
+            } else if(this.classData[i].photoPath === "drawingImg"){
+              this.classData[i].photoPath = this.drawingImg;
+            } else if(this.classData[i].photoPath === "writingImg"){
+              this.classData[i].photoPath = this.writingImg;
+            } else if(this.classData[i].photoPath === "readingImg"){
+              this.classData[i].photoPath = this.readingImg;
+            } else if(this.classData[i].photoPath === "studyImg"){
+              this.classData[i].photoPath = this.studyImg;
+            } else if(this.classData[i].photoPath === "foreignImg"){
+              this.classData[i].photoPath = this.foreignImg;
+            } else if(this.classData[i].photoPath === "dogImg"){
+              this.classData[i].photoPath = this.dogImg;
+            } else if(this.classData[i].photoPath === "donationImg"){
+              this.classData[i].photoPath = this.donationImg;
+            } else if(this.classData[i].photoPath === "cookingImg"){
+              this.classData[i].photoPath = this.cookingImg;
+            } else if(this.classData[i].photoPath === "dessertImg"){
+              this.classData[i].photoPath = this.dessertImg;
+            } else if(this.classData[i].photoPath === "sojuImg"){
+              this.classData[i].photoPath = this.sojuImg;
+            } 
+          }
         } else {
           this.isEmpty = true;
         }
